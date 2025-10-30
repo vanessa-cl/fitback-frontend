d4489d47942c1cd47eb3d366491f8d435e4a6f13
vanessa-cl <vanessalima951@gmail.com>
2025-10-29 21:54:27 -0300

    fix: correção na importação do arquivo de rotas

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import customTheme from "./styles/theme.js";
-import Router from "./routes/Router.jsx";
+import Router from "./routes/routes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
